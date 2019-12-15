3395ea81f4177b67994a72e2c23dbb320f4d879b
jeanwei <jeanwei10292002@gmail.com>
2019-12-15 14:13:38 +0800

    12/15 劍道slider

--- demo-slider/ViewController.swift
+++ demo-slider/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var grassland: UIImageView!
+    @IBOutlet weak var burntegg: UIImageView!
+    @IBOutlet weak var quarteregg: UIImageView!
+    @IBOutlet weak var beategg: UIImageView!
+    @IBOutlet weak var layegg: UIImageView!
+    @IBOutlet weak var eggLabel: UILabel!
+    @IBOutlet weak var eggSlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        grassland.transform = CGAffineTransform.identity.translatedBy(x: 0.5, y: -60).scaledBy(x: 6, y: 4)
     }
 
-
+    @IBAction func eggSlider(_ sender: UISlider) {
+        burntegg.frame.origin.x = CGFloat(10+110 * sender.value)
+        layegg.frame.origin.x = CGFloat(700-110*sender.value)
+    }
+    
 }
 
